dff2bf87c5b3a132bc281046e1be2eb394655962
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-09-02 23:14:17 +0100

    feat: terminando o componente de detalhes de cesta

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import { View, StatusBar, SafeAreaView } from 'react-native';
 import { Basket } from './src/views/basket';
+import { 
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat'
 
 export default function App() {
+  const [ myFonts ] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  })
+
+  if(!myFonts) return <View/>
+
   return (
     <SafeAreaView>
       <StatusBar/>
--- src/views/basket.js
+++ src/views/basket.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Image, StyleSheet, Dimensions, Text } from 'react-native'
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native'
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width
 export const Basket = () => {
@@ -8,6 +9,19 @@ export const Basket = () => {
     <>
       <Image source={topo} style={styles.top}/>
       <Text style={styles.title}>Detalhe da cesta</Text>
+      <View style={styles.BasketContainer}>
+        <Text style={styles.name}>Cesta de Verduras</Text>
+        <View style={styles.farmContainer}>
+          <Image source={logo} style={styles.farmImage} />
+          <Text style={styles.farmName}>Jenny Jack Farms</Text>
+        </View>
+        <Text style={styles.description}>
+            Uma cesta com produtos selecionados
+            cuidadosamente da fazenda
+            direito para sua cozinha
+        </Text>
+        <Text style={styles.price}>AOA 40,00</Text>
+      </View>
     </>
   )
 }
@@ -15,7 +29,7 @@ export const Basket = () => {
 const styles = StyleSheet.create({
   top:{
     width: '100%',
-    height: 578 / 768 * width
+    height: 578 / 768 * width,
   },
   title: {
     position: 'absolute',
@@ -26,5 +40,42 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     padding: 16
+  },
+  name: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: '#464646',
+    fontFamily: 'Montserrat_700Bold'
+  },
+  BasketContainer:{
+    paddingVertical: 8,
+    paddingHorizontal: 16
+  },
+  farmImage: {
+    width: 32,
+    height: 32
+  },
+  farmName: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: 'Montserrat_400Regular'
+  },
+  farmContainer:{
+    flexDirection: 'row',
+    paddingVertical: 12,
+    alignItems: 'center'
+  },
+  description: {
+    color: '#A3A3A3',
+    fontSize: 16,
+    lineHeight: 26
+  },
+  price: {
+    color: '#2A9F85',
+    fontWeight: 'bold',
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8
   }
 })
